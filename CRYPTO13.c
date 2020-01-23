8b41e1c194ce073738091da66129f3716d7af6a7
Spyros Mouchlianitis <56831313+SpyrosMouchlianitis@users.noreply.github.com>
2020-01-23 18:08:32 +0200

    Add files via upload

---
+++ CRYPTO13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char crypto(char c){
+
+    if (c >='a' && c <= 'z')
+    {
+        c -= 'a';
+        c = (c+13)%26;
+        c += 'a';
+    }
+        
+    if (c >='A' && c <= 'Z')
+    {
+        c -= 'A';
+        c = (c+13)%26;
+        c += 'A';
+    }
+
+    return c;
+}
+
+
+int main(){
+
+    int c;
+    while ( (c=getchar()) != EOF)
+    {
+        printf("%c\n", crypto(c));
+    }
+    
+
+    return 0;
+}
